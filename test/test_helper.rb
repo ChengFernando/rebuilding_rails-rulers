8637e733986797c50386dc355e3ecbe00a048783
ChengFernando <fernandocheng2010@gmail.com>
2016-03-17 10:35:57 +0800

    CH1 ex3 Test Early, Test Often, add test

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,7 @@
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'rack/test'
+require 'test/unit'
+
+d = File.join(File.dirname(__FILE__), "..", "lib")
+$LOAD_PATH.unshift File.expand_path(d)
 require 'rulers'
 
-require 'minitest/autorun'
