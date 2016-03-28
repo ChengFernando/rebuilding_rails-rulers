2709c2d7641cfaa2dd448eff5f012d68892ac742
ChengFernando <fernandocheng2010@gmail.com>
2016-03-28 10:40:45 +0800

    CH5 Basic Models - 2

--- lib/rulers/dependencies.rb
+++ lib/rulers/dependencies.rb
@@ -2,7 +2,7 @@ class Object
   @monitor = Monitor.new
   @looking_for_consts = {}
 
-  def self.const_missing c
+  def self.const_missing(c)
     @monitor.synchronize do
       return nil if @looking_for_consts[c]
       @looking_for_consts[c] = true
